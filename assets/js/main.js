9f8dff608fdd5aeeaaf4cbb727e7cad24919d2b5
vainawebm <vainaweb@gmail.com>
2020-07-22 16:35:59 -0300

    arrays de músicas e artistas criados

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+/*  Exercícios sovbre arrays */
+
+/*  1 - Crie uma array com suas musicas favoritas
+  (ao menos 5) */
+var favoriteMusic = ['The Scientist', 'Back At One', 'Light', 'Photograph', 'Ironic'] 
+
+/*  2 - Retire a primeira música e guarde em uma variável */
+
+/*  3 - Retire a ultima música e guarde em uma variável */
+
+/*  4 - Crie uma segundo array com os nomes dos cantores
+  e junte ele com o array de musicas */
+
+  var artists = ['Coldplay', 'Brian McNight', 'Sleeping At Last', 'Nickelback', 'Alanis Morissette']
+
+/*  5 - Encontre a música com o maior nome dentro do array */
